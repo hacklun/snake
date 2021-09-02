c1b64e1c2ff8ed5e0fe22c4f6e8c857964138f21
hacklun <hacklun@mail.ru>
2021-09-02 15:20:34 +0300

    add board

--- app.js
+++ app.js
@@ -1,5 +1,13 @@
 const board = document.getElementById('board');
+const WIDTH = 10;
 
-createBoard = () => {
+let snake = [0, 1, 2];
 
+const createBoard = () => {
+    for (let i = 0; i < WIDTH * WIDTH; i++) {
+        const cell = document.createElement('div');
+        board.appendChild(cell);
+    }
 }
+
+createBoard();
