7cadf7b6b858aea2d4ab875635ccb65bec5c2a58
hacklun <hacklun@mail.ru>
2021-09-07 20:41:16 +0300

    add function drawsnake

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const board = document.getElementById('board');
+const cells = [];
 const WIDTH = 10;
 
 let snake = [0, 1, 2];
@@ -7,7 +8,14 @@ const createBoard = () => {
     for (let i = 0; i < WIDTH * WIDTH; i++) {
         const cell = document.createElement('div');
         board.appendChild(cell);
+        cells.push(cell);
     }
+}
+
+const drawSnake = () => {
+    cells.forEach((cell, index) => {
+
+    });
 }
 
 createBoard();
